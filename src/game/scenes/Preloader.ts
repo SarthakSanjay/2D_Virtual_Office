c01697a84b2712581ee87bc0b65cde8746d90141
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-11-20 23:37:18 +0530

    preloaded character assets

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -13,6 +13,15 @@ export class Preloader extends Scene {
         this.load.tilemapTiledJSON('room', 'interior/room.json')
 
         //man character
+        this.load.atlas('man', 'character/idle.png', 'character/idle.json')
+
+        this.load.atlas('run', 'character/run.png', 'character/run.json')
+
+        this.load.atlas('man-phone', 'character/man_phone.png', 'character/man_phone.json')
+
+        this.load.atlas('man-sit1', 'character/sit1.png', 'character/sit1.json')
+        this.load.atlas('man-sit2', 'character/sit2.png', 'character/sit2.json')
+        this.load.atlas('man-sit3', 'character/sit3.png', 'character/sit3.json')
 
     }
 
