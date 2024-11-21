da557166596cee2075f635c51488252999c71556
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-11-21 23:25:59 +0530

    preloaded character sitting atlas

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -10,18 +10,20 @@ export class Preloader extends Scene {
 
     preload() {
         this.load.image('tiles', 'interior/room_extruded.png')
+        this.load.image('outside', 'interior/outside_extruded.png')
         this.load.tilemapTiledJSON('room', 'interior/room.json')
 
+
         //man character
-        this.load.atlas('man', 'character/idle.png', 'character/idle.json')
+        this.load.atlas('idle', 'character/idle.png', 'character/idle.json')
 
         this.load.atlas('run', 'character/run.png', 'character/run.json')
 
         this.load.atlas('man-phone', 'character/man_phone.png', 'character/man_phone.json')
 
-        this.load.atlas('man-sit1', 'character/sit1.png', 'character/sit1.json')
-        this.load.atlas('man-sit2', 'character/sit2.png', 'character/sit2.json')
-        this.load.atlas('man-sit3', 'character/sit3.png', 'character/sit3.json')
+        this.load.atlas('sit', 'character/sit1.png', 'character/sit1.json')
+        this.load.atlas('sit2', 'character/sit2.png', 'character/sit2.json')
+        this.load.atlas('sit3', 'character/sit3.png', 'character/sit3.json')
 
     }
 
