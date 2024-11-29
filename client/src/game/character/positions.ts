7a55f32b078e89ef1e071ece5630536108f167bc
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-11-29 22:25:25 +0530

    added all the tiles coordinate where character have to sit

--- client/src/game/character/positions.ts
+++ client/src/game/character/positions.ts
@@ -11,13 +11,131 @@ export const sofaTiles = [
         y: 10
     },
     {
+        side: 'left',
+        x: 10,
+        y: 30
+    },
+    {
+        side: 'left',
+        x: 10,
+        y: 32
+    }, {
+        side: 'left',
+        x: 10,
+        y: 34
+    }, {
+        side: 'left',
+        x: 10,
+        y: 36
+    }, {
+        side: 'left',
+        x: 7,
+        y: 30
+    }, {
+        side: 'left',
+        x: 7,
+        y: 32
+    }, {
+        side: 'left',
+        x: 7,
+        y: 34
+    }, {
+        side: 'left',
+        x: 7,
+        y: 36
+    }, {
+        side: 'left',
+        x: 39,
+        y: 11
+    }, {
         side: 'right',
-        x: 27,
+        x: 45,
+        y: 11
+    },
+    {
+        side: 'right',
+        x: 26,
         y: 9
     },
     {
         side: 'right',
-        x: 27,
+        x: 26,
         y: 10
-    }
+    },
+    {
+        side: 'up',
+        x: 23,
+        y: 8
+    },
+    {
+        side: 'up',
+        x: 24,
+        y: 8
+    },
+    {
+        side: 'up',
+        x: 40,
+        y: 10
+    }, {
+        side: 'up',
+        x: 42,
+        y: 10
+    }, {
+        side: 'up',
+        x: 44,
+        y: 10
+    }, {
+        side: 'up',
+        x: 46,
+        y: 10
+    },
+    {
+        side: 'down',
+        x: 23,
+        y: 11
+    }, {
+        side: 'down',
+        x: 24,
+        y: 11
+    }, {
+        side: 'down',
+        x: 40,
+        y: 13
+    }, {
+        side: 'down',
+        x: 42,
+        y: 13
+    }, {
+        side: 'down',
+        x: 44,
+        y: 13
+    },
+]
+
+export const sofaVertex = [
+    { x: 20, y: 8 },
+    { x: 27, y: 8 },
+    { x: 20, y: 12 },
+    { x: 27, y: 8 },
+]
+
+export const chairVertex = [
+    { x: 39, y: 10 },
+    { x: 45, y: 10 },
+    { x: 39, y: 14 },
+    { x: 45, y: 14 }
+]
+
+export const gardenVertex = [
+    { x: 29, y: 27 },
+    { x: 52, y: 27 },
+    { x: 29, y: 42 },
+    { x: 52, y: 42 }
+]
+
+export const presentationRoomVertex = [
+    { x: 6, y: 30 },
+    { x: 12, y: 30 },
+    { x: 6, y: 38 },
+    { x: 12, y: 38 },
 ]
