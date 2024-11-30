92f0dff2083e86ca8f5408998a305750474db552
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-11-30 23:38:12 +0530

    created dashboard component

---
+++ client/src/components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import Navbar from './Navbar';
+import Space from './Space';
+
+const Dashboard = () => {
+    return (
+        <div className="flex flex-col h-screen w-screen bg-blue-800">
+            <Navbar />
+            <Space />
+        </div>
+    );
+};
+
+export default Dashboard;
