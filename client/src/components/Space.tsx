91bc6e0086bfbcfac94b56bfe551cac0d338b6e4
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-11-30 23:38:36 +0530

    created space component

---
+++ client/src/components/Space.tsx
@@ -0,0 +1,10 @@
+const Space = () => {
+    return (
+        <div className="h-full w-full bg-blue-900 flex justify-center items-center">
+            {/* Space content */}
+            <p>Space content goes here</p>
+        </div>
+    );
+};
+
+export default Space;
