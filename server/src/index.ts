ca665e4f8ad282756f5f341efaaf34f3bbdf98e7
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-12-19 23:41:04 +0530

    test post route to create space

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,41 @@
-import express from "express"
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
+const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'hello' })
-})
+// Middleware to parse JSON request body
+app.use(express.json());
 
-app.post('/create_space', (req, res) => {
-  const { space_name } = req.body
-  // generate a unique id for space 
-  // put this data in the database
+// app.get("/", (req, res) => {
+// res.json({ msg: "hello" });
+// });
 
-  res.json({ msg: 'space created' })
-})
+app.post("/create_space", async (req: any, res: any) => {
+  const { space_name } = req.body;
+  // Check if space_name is provided
+  if (!space_name) {
+    return res.status(400).json({ msg: "space name not sent correctly" });
+  }
+
+  try {
+    // Create the space in the database
+    const space = await prisma.space.create({
+      data: {
+        name: space_name,
+      },
+    });
+
+    console.log(space)
+    res.json({ msg: "space created", space });
+  } catch (error) {
+    console.error("Error creating space:", error);
+    res.status(500).json({ msg: "Error creating space" });
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
-}
-)
+  console.log(`Server running on port ${PORT}`);
+});
+
