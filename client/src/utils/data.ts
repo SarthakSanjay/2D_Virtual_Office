6df721dbd32aa1299e83ec4c5692d215ccae74dd
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-12-20 23:50:09 +0530

    added data seperately

---
+++ client/src/utils/data.ts
@@ -0,0 +1,22 @@
+export const slides = [
+    {
+        id: 1,
+        image: 'https://cdn.gather.town/v0/b/gather-town.appspot.com/o/manually-uploaded%2Fvalueprop%2Fvalueprop1.png?alt=media'
+        ,
+        text: "Let's create you a office environment"
+    }, {
+        id: 2,
+        image: 'https://cdn.gather.town/v0/b/gather-town.appspot.com/o/manually-uploaded%2Fvalueprop%2Fvalueprop2.png?alt=media',
+        text: 'Talk naturally throughout the day'
+    }, {
+        id: 3,
+        image: 'https://cdn.gather.town/v0/b/gather-town.appspot.com/o/manually-uploaded%2Fvalueprop%2Fvalueprop3.png?alt=media',
+        text: 'See what your teammates are up to'
+    }, {
+        id: 4,
+        image: 'https://cdn.gather.town/v0/b/gather-town.appspot.com/o/manually-uploaded%2Fvalueprop%2Fvalueprop5.png?alt=media',
+        text: 'Your turn! Let’s create your virtual HQ'
+    },
+]
+
+
