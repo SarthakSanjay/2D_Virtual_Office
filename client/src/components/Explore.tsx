adfcc8ae6772656df01a5c2ef29eb53734e4b871
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-12-22 23:06:17 +0530

    refactored

--- client/src/components/Explore.tsx
+++ client/src/components/Explore.tsx
@@ -3,8 +3,12 @@ import { slides } from '../utils/data'
 import { useAuth0 } from '@auth0/auth0-react'
 import { useNavigate } from 'react-router'
 import axios from 'axios'
+import CreateOffice from './CreateOffice'
+import { MdCancel } from 'react-icons/md'
 
-const Explore = () => {
+const Explore = ({ setClickedCreateSpaceBtn }: {
+    setClickedCreateSpaceBtn: (value: boolean) => void
+}) => {
     const [id, setId] = useState(1)
     const [space, setSpace] = useState('')
     const { user, isAuthenticated, loginWithPopup } = useAuth0()
@@ -17,7 +21,10 @@ const Explore = () => {
             console.log('create space')
             // navigate('/app')
             await axios.post('http://localhost:3000/create_space', {
-                name: space
+                space_name: space,
+                username: user?.name,
+                email: user?.email,
+                image: user?.picture
             }).then(() => {
                 alert('space created')
             }).catch((e) => {
@@ -25,40 +32,23 @@ const Explore = () => {
             })
         }
     }
-
-    if (isAuthenticated) {
-        navigate('/app')
-    }
+    //may be use useEffect 
+    // if (isAuthenticated) {
+    //     navigate('/app')
+    // }
 
     if (id === 4) {
-        return <div className='h-screen w-screen flex justify-center items-center flex-col'>
-            <h1 className='text-xl my-10'>Create a new office space for your team</h1>
-            <p className='text-sm w-1/3 text-start'>Space name will appear at the end of url</p>
-            <input
-                className='h-10 w-1/3 border-2 border-white bg-transparent rounded-lg outline-none px-3'
-                onChange={(e) => {
-                    setSpace(e.target.value)
-                }}
-                value={space}
-            />
-            <div className='w-1/3 flex justify-between my-10'>
-                <button
-                    className='h-10 w-max px-5 rounded-lg bg-white text-black'
-                    onClick={() => setId(p => p - 1)}
-                >Back</button>
-                <button
-                    className='h-10 w-max px-5 rounded-lg bg-green-600 text-black disabled:bg-green-600/60'
-                    disabled={!space}
-                    onClick={handleCreateSpace}
-                >Create space</button>
-
-            </div>
-        </div>
+        return <CreateOffice
+            setId={setId}
+            setSpace={setSpace}
+            space={space}
+            handleCreateSpace={handleCreateSpace}
+        />
     }
 
     return (
         <div className='h-3/4 w-3/4 rounded-lg bg-black p-4
-            space-y-10'>
+            space-y-10 relative'>
             <img src={slides[id - 1].image} className='h-[80%] w-full rounded-lg' />
             <div className='h-[10%] w-full rounded-lg flex justify-between items-center '>
                 <h1 className='text-xl'>{slides[id - 1].text}</h1>
@@ -83,6 +73,9 @@ const Explore = () => {
                     </div>
                 }
             </div>
+            <button onClick={() => setClickedCreateSpaceBtn(false)}
+                className="h-10 w-10 text-3xl rounded-full absolute top-[-15%] right-[-5%] flex justify-center items-center bg-white/25 text-black"><MdCancel /></button>
+
         </div>
     )
 }
