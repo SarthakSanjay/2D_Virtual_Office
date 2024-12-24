02db1c030718fafa93e2daf8ec1a2ea557081191
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-12-24 23:47:11 +0530

    added navigation link to space

--- client/src/components/Space.tsx
+++ client/src/components/Space.tsx
@@ -1,12 +1,31 @@
 import { BsThreeDotsVertical } from "react-icons/bs";
 import Explore from "./Explore";
 import Backdrop from "./Backdrop";
+import axios from 'axios'
+import { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router";
 
 const Space = ({ clickedCreateSpaceBtn, setClickedCreateSpaceBtn }: {
     clickedCreateSpaceBtn: boolean,
     setClickedCreateSpaceBtn: (value: boolean) => void
 }) => {
+    const { user } = useAuth0()
+    const [spaces, setSpaces] = useState([])
+    useEffect(() => {
+        async function getSpaces() {
+            const res = await axios.get('http://localhost:3000/spaces', {
+                params: {
+                    email: user?.email
+                }
+            })
+            setSpaces(res.data.spaces)
+        }
 
+        getSpaces()
+    }, [])
+
+    console.log(spaces)
     if (clickedCreateSpaceBtn) {
         return <Backdrop>
             <Explore setClickedCreateSpaceBtn={setClickedCreateSpaceBtn} />
@@ -28,22 +47,30 @@ const Space = ({ clickedCreateSpaceBtn, setClickedCreateSpaceBtn }: {
                     className="px-4 py-2 rounded-lg bg-transparent border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
                 />
             </div>
+            <div
+                className="h-full w-full px-10 py-2 flex gap-10 flex-wrap">
+                {spaces.map((space: { name: string, id: string }) => {
+                    return <Link
+                        to={`/space/${space.id}/${space.name}`}
+                        key={space.id}
+                        className="h-52 w-72 rounded-lg bg-transparent"
+                    >
+                        <div className="h-40 w-full border rounded-lg"></div>
 
-            <div className="h-full w-full px-10 py-2">
-                <div className="h-52 w-72 rounded-lg bg-transparent">
-                    <div className="h-40 w-full border rounded-lg"></div>
+                        <div className="h-12 w-full flex justify-between items-center px-1">
+                            <div className="text-sm font-medium text-white">{space.name}</div>
 
-                    <div className="h-12 w-full flex justify-between items-center px-1">
-                        <div className="text-sm font-medium text-white">Name</div>
-
-                        <div className="flex items-center space-x-2">
-                            <span className="text-gray-400 text-sm">Last Visited</span>
-                            <button className="h-5 w-5 flex justify-center items-center rounded-sm hover:bg-black/50">
-                                <BsThreeDotsVertical />
-                            </button>
+                            <div className="flex items-center space-x-2">
+                                <span className="text-gray-400 text-sm">Last Visited</span>
+                                <button className="h-5 w-5 flex justify-center items-center rounded-sm hover:bg-black/50">
+                                    <BsThreeDotsVertical />
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    </Link>
+
+                })}
+
             </div>
         </div>
     );
