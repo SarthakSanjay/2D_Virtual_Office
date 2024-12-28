6d1130af18ebfc44ae8a01106355943824514677
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-12-28 23:33:19 +0530

    handel dropdown toggles

--- client/src/components/BottomBar.tsx
+++ client/src/components/BottomBar.tsx
@@ -1,16 +1,36 @@
+import { useState } from "react"
 import { BsEmojiSmileFill } from "react-icons/bs"
-import { FaChevronUp, FaVideoSlash } from "react-icons/fa"
+import { FaChevronUp, FaDoorOpen, FaVideoSlash } from "react-icons/fa"
 import { IoMdMicOff } from "react-icons/io"
-import { MdEdit, MdScreenShare } from "react-icons/md"
+import { IoChatbubbles } from "react-icons/io5"
+import { MdEdit, MdScreenShare, MdSupervisorAccount } from "react-icons/md"
 import { VscAccount } from "react-icons/vsc"
+import MainMenuDropdown from "./Dropdown/MainMenuDropdown"
+import ProfileMenuDropdown from "./Dropdown/ProfileMenuDropdown"
 
 const BottomBar = () => {
+    const [toggleMainMenuDD, setToggleMainMenuDD] = useState<boolean>(false)
+    const [toggleProfileMenuDD, setToggleProfileMenuDD] = useState<boolean>(false)
+
+    const handleMainMenuToggle = () => {
+        setToggleMainMenuDD(!toggleMainMenuDD);
+        if (toggleProfileMenuDD) setToggleProfileMenuDD(false);
+    };
+
+    const handleProfileMenuToggle = () => {
+        setToggleProfileMenuDD(!toggleProfileMenuDD);
+        if (toggleMainMenuDD) setToggleMainMenuDD(false);
+    };
     return (
-        <div className='h-12 w-full bg-customDarkGreen flex justify-between items-center'>
+        <div className='h-12 w-full bg-customDarkGreen flex justify-between items-center relative'>
+            {toggleMainMenuDD && <MainMenuDropdown />}
+            {toggleProfileMenuDD && <ProfileMenuDropdown />}
             {/* left side */}
             <div className="h-12 w-max px-2 flex items-center gap-2">
                 {/* main menu button */}
-                <button className="h-10 w-10 bg-customLightGreen/50 text-customTextGreen hover:bg-customDarkGreen hover:text-customLightGreen grid place-content-center rounded-lg">
+                <button className="h-10 w-10 bg-customLightGreen/50 text-customTextGreen hover:bg-customDarkGreen hover:text-customLightGreen grid place-content-center rounded-lg"
+                    onClick={handleMainMenuToggle}
+                >
                     VQ
                 </button>
                 {/* personal setting & edit */}
@@ -18,7 +38,8 @@ const BottomBar = () => {
                     <button className="h-10 w-14 bg-customLightGreen/50 text-customTextGreen hover:bg-customDarkGreen hover:text-customLightGreen border-r border-customDarkGreen grid place-content-center rounded-l-lg">
                         <VscAccount />
                     </button>
-                    <button className="h-10 w-44 px-2 bg-customLightGreen/50 text-customTextGreen hover:bg-customDarkGreen hover:text-customLightGreen flex flex-col  rounded-r-lg relative">
+                    <button className="h-10 w-44 px-2 bg-customLightGreen/50 text-customTextGreen hover:bg-customDarkGreen hover:text-customLightGreen flex flex-col  rounded-r-lg relative"
+                        onClick={handleProfileMenuToggle}>
                         <div className="w-full flex justify-between items-center">
                             <h1 className="text-sm">username</h1>
                             {/* edit */}
@@ -67,7 +88,26 @@ const BottomBar = () => {
                 </button>
 
             </div>
-            <div></div>
+            {/* right */}
+            <div className="h-12 w-max px-2 flex items-center gap-2">
+
+
+                <button className="h-9 w-9 bg-customLightGreen/50 text-customTextGreen hover:bg-customDarkGreen hover:text-customLightGreen grid place-content-center rounded-lg">
+                    <IoChatbubbles />
+                </button>
+                <button className="h-9 w-28 bg-customLightGreen/50 text-customTextGreen hover:bg-customDarkGreen hover:text-customLightGreen flex justify-center items-center text-lg gap-2 rounded-lg">
+                    <div className="text-[25px]">
+                        <MdSupervisorAccount />
+                    </div>
+                    <div className="h-[10px] w-[10px]  rounded-full bg-emerald-300"></div>
+                    <h1>1</h1>
+                </button>
+
+                <button className="h-9 w-9 bg-customLightGreen/50 text-customTextGreen hover:bg-[#F5004F] grid place-content-center rounded-lg">
+                    <FaDoorOpen />
+                </button>
+            </div>
+
 
         </div>
     )
